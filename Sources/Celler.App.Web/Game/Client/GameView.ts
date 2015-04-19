d26ea85ea975b48b003dc652af6aef9d8432df38
Roman Koshelev <romakosh@gmail.com>
2015-04-19 13:27:55 +0300

    CN-115 fix: images

--- Sources/Celler.App.Web/Game/Client/GameView.ts
+++ Sources/Celler.App.Web/Game/Client/GameView.ts
@@ -18,7 +18,7 @@ module Celler {
         }
 
         create() {
-            var logo = this.game.add.sprite( this.game.world.centerX, this.game.world.centerY, "logo" );
+            var logo = this.game.add.sprite( this.game.world.centerX, this.game.world.centerY, "sample" );
 
             logo.anchor.setTo( 0.5, 0.5 );
             logo.scale.setTo( 0.2, 0.2 );
