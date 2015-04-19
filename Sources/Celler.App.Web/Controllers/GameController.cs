d598bce9044d463544fa9a4c454d2730cb2d989f
Roman Koshelev <romakosh@gmail.com>
2015-04-19 10:19:19 +0300

    update

---
+++ Sources/Celler.App.Web/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿// Celler (c) 2015 Krokodev
+// Celler.App.Web
+// HomeController.cs
+
+using System.Web.Mvc;
+
+namespace Celler.App.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Sources/Celler.App.Web/Controllers/HomeController.cs
+++ Sources/Celler.App.Web/Controllers/HomeController.cs
@@ -4,18 +4,15 @@
 
 using System.Web.Mvc;
 
+// Here: Controller : Home 
+
 namespace Celler.App.Web.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return RedirectToAction( "Game" );
-        }
-
-        public ActionResult Game()
-        {
-            return View();
+            return RedirectToAction( "Index", "Game" );
         }
 
         public ActionResult Chat()
